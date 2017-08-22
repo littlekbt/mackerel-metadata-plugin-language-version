7ed2618d609f82f7db5d572455330cfbed13f14f
littlekbt <kr.kubota.11@gmail.com>
2017-08-22 14:32:05 +0900

    fix loop

--- language_versions.rb
+++ language_versions.rb
@@ -7,8 +7,9 @@ class LanguageVersions
     def fetch(targets=[])
       (Dir.glob((Pathname(File.expand_path(File.dirname(__FILE__))) + 'config'+'*json').to_s)).inject({}) do |conf, filename|
         conf.merge(JSON.parse(File.read(filename)))
+      end.select do |key, conf|
+        targets.empty? || targets.include?(key)
       end.inject([]) do |result, (key, conf)|
-        next result if !(targets.empty? || targets.include?(key))
         result.push(key => parse(execute(conf['version']), conf['parser']))
       end
     end
